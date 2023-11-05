81818ca4a1065bcf1e82c10b9cf40e548e2d04b4
MOIZ RASHEED <moizrasheed.01@gmail.com>
2023-11-05 06:21:12 +0500

    Header duration adjustment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button() {
   return (
-    <button className="bg-[#1291ee] hover:bg-[#0f83d7] duration-200 px-6 py-3 text-sm text-white">
+    <button className="bg-[#1291ee] hover:bg-[#0f83d7] duration-200 px-6 py-3 rounded-sm text-sm text-white">
       Get Started
     </button>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,8 +28,8 @@ function Header() {
         <div className="flex items-center justify-between py-4">
           <Logo />
           <ul
-            className={`md:flex gap-6 text-sm md:bg-transparent md:w-auto md:static md:px-0 absolute top-[100%] w-44 bg-white px-1 rounded-sm ${
-              open ? "right-[2%] duration-500" : "right-[-100%]"
+            className={`md:flex gap-6 text-sm md:bg-transparent md:w-auto md:static md:px-0 absolute top-[100%] w-44 bg-white px-1 rounded-sm duration-500 ${
+              open ? "right-[4%]" : "right-[-100%]"
             }`}
           >
             {navItems.map((item) => (
