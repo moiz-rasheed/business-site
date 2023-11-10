68c08e6d02f49de0417d201e884ee672f692be21
MOIZ RASHEED <moizrasheed.01@gmail.com>
2023-11-11 01:38:36 +0500

    Fix: Header text color on scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -41,12 +41,12 @@ function Header() {
   return (
     <header
       className={`fixed w-full top-0 right-0 z-[1000] bg-transparent ${
-        isScrolled ? "backdrop-blur duration-200" : ""
+        isScrolled ? "bg-white shadow duration-200" : ""
       }`}
     >
       <Container>
         <div className="flex items-center justify-between py-4">
-          <Logo />
+          <Logo textColor={`${isScrolled ? "text-black" : "text-white"}`} />
           <ul
             className={`md:flex gap-6 text-sm md:bg-transparent md:w-auto md:px-0 absolute md:static drop-shadow-lg md:drop-shadow-none top-[100%] w-44 bg-white px-1 rounded-sm duration-500 ${
               open ? "right-4" : "right-[-100%]"
@@ -57,7 +57,11 @@ function Header() {
                 key={item.name}
                 className="md:bg-transparent md:hover:bg-transparent hover:bg-[#1291ee] rounded-sm md:my-0 my-1 md:p-0 p-2 md:text-white text-black hover:text-white"
               >
-                <p className="md:font-extralight font-light md:hover:underline underline-offset-4 block cursor-pointer">
+                <p
+                  className={`md:font-extralight font-light md:hover:underline underline-offset-4 block cursor-pointer ${
+                    isScrolled ? "md:text-black md:font-normal" : ""
+                  }`}
+                >
                   {item.name}
                 </p>
               </li>
@@ -67,7 +71,9 @@ function Header() {
             <Button />
             <div
               onClick={() => setOpen(!open)}
-              className="text-3xl text-white ml-2 cursor-pointer z-[10001] md:hidden"
+              className={`text-3xl  ml-2 cursor-pointer z-[10001] md:hidden ${
+                isScrolled ? "text-black" : "text-white"
+              }`}
             >
               <ion-icon name={open ? "close-sharp" : "menu-sharp"}></ion-icon>
             </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import LogoSvg from "../assets/Logo.svg";
 
-function Logo() {
+function Logo({ textColor = "text-white" }) {
   return (
-    <div className="flex items-center text-white cursor-pointer">
+    <div className={`flex items-center cursor-pointer ${textColor}`}>
       <img src={LogoSvg} alt="Logo" className="w-6" />
-      <p className="ml-1 text-lg font-extralight">
+      <p className="ml-1 text-lg font-light">
         Your<span className="font-medium">Logo.</span>
       </p>
     </div>
